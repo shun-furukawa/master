e6f6a71a6004b272be94f4cddcefcd68cadeee5e
古川俊 <furukawashun@furukawashunnomacbook-pro.local>
2019-03-31 23:01:04 +0900

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by 古川俊 on 2019/03/31.
+//  Copyright © 2019年 古川俊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
